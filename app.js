f62222dc6b7d064d75b3406c349d4b872a60cbdf
Michael McBride <michaelmcbride113@gmail.com>
2025-01-29 16:56:25 -0500

    Updated lab

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 const displayElement = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
 const buttonNumbers = document.querySelectorAll(".button.number");
-const buttonOperators = document.querySelectorAll(".button.operators");
+const buttonOperators = document.querySelectorAll(".button.operator");
 const buttonEquals = document.querySelector(".button.equals");
 const buttonClear = document.querySelector(".button.clear");
 const calculator = document.querySelector('.calculator');
 const handleInput = (event) => {
 };
 const maths = (number1, number2, operator) => {
-    const math = {
+  const math = {
     '*': (x, y) => x * y,
     '/': (x, y) => x / y,
     '+': (x, y) => x + y,
     '-': (x, y) => x - y,
-    };
-    return math[operator](+number1, +number2);
+  };
+  return math[operator](+number1, +number2);
 };
-    
+
 
 
 /*-------------------------------- Variables --------------------------------*/
@@ -29,47 +29,55 @@ let previousInput = "";
 
 /*----------------------------- Event Listeners -----------------------------*/
 buttonEquals.addEventListener('click', () => {
-    if(currentInput !== "" && previousInput !== "") {
-        previousInput = maths(previousInput, currentInput, operator);
-        displayElement.textContent = previousInput;
-        operator = "";
-        currentInput = "";
-    }
-});
+  console.log(currentInput)
+  console.log(operator)
+  console.log(previousInput)
+  if (currentInput !== "") {
+    if (previousInput !== "") {
+      if (operator !== "") {
+
+      displayElement.textContent = maths(previousInput, currentInput, operator);
+      operator = "";
+      currentInput = "";
+    } console.log(buttonEquals)
+  }
+}});
 
 buttonClear.addEventListener('click', () => {
-    currentInput = "";
-    previousInput = "";
-    operator = "";
-    displayElement.textContent = "";  
+  currentInput = "";
+  previousInput = "";
+  operator = "";
+  displayElement.textContent = "";
 });
 // /*-------------------------------- Functions --------------------------------*/
 buttonNumbers.forEach((num) => {
-    num.addEventListener('click', () => {
-    currentInput += num.innerText;
+  num.addEventListener('click', () => {
+    currentInput = num.innerText;
     displayElement.textContent = currentInput;
-    });
+  });
 });
 buttonOperators.forEach((op) => {
-    op.addEventListener('click', () => {
-        if (currentInput !== "") {
-        if (previousInput !== "") {
-          previousInput = maths(previousInput, currentInput, operator); // Perform the operation
-        } else {
-          previousInput = currentInput;
-        }
+  op.addEventListener('click', () => {
+    if (currentInput !== "") {
+      if (previousInput !== "") {
+        if (operator !== "")
+        previousInput = maths(previousInput, currentInput, operator);
+      } else {
+        previousInput = currentInput;
       }
-      operator = op.innerText;
-      currentInput = "";
-      displayElement.textContent = previousInput;
-    });
+    }
+    operator = op.innerText;
+    displayElement.textContent = previousInput;
+    console.log(op)
+    
   });
-  
+});
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      console.log(event.target.innerText);
-    });
-  });
-  
+
+// buttons.forEach((button) => {
+//   button.addEventListener('click', (event) => {
+//     console.log(event.target.innerText);
+//   });
+// });
+
 
